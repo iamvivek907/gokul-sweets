4829ccae6722c9a87c67f8f4b2085b57acad7567
iamvivek907 <78471513+iamvivek907@users.noreply.github.com>
2026-09-26 18:13:49 +0530

    SCRUM-95 document and test checkout rollout gate

--- backend/src/test/java/com/gokulsweets/restaurant/config/StorefrontFeaturesControllerTest.java
+++ backend/src/test/java/com/gokulsweets/restaurant/config/StorefrontFeaturesControllerTest.java
@@ -26,6 +26,31 @@ class StorefrontFeaturesControllerTest {
     }
 
     @Test
+    void checkoutExperienceRequiresAllCommitmentAndPaymentSafeguards() {
+        var properties = new EnhancementProperties();
+        properties.setCheckoutExperienceV2(true);
+        var controller = new StorefrontFeaturesController(properties, clock);
+        assertThat(controller.features().checkoutExperienceV2()).isFalse();
+
+        properties.setSmartAvailability(true);
+        properties.setSmartPickupSelection(true);
+        properties.setAuthoritativePickupCommitment(true);
+        properties.setPersistentPickupContext(true);
+        properties.setCartSwitchPreview(true);
+        properties.setInPlaceBranchSwitch(true);
+        properties.setAcceptedCheckoutQuote(true);
+        properties.setAccessibleOrderingV2(true);
+        properties.setPaymentPollingV2(true);
+        assertThat(controller.features().checkoutExperienceV2()).isTrue();
+
+        properties.setAcceptedCheckoutQuote(false);
+        assertThat(controller.features().checkoutExperienceV2()).isFalse();
+        properties.setAcceptedCheckoutQuote(true);
+        properties.setCheckoutExperienceV2(false);
+        assertThat(controller.features().checkoutExperienceV2()).isFalse();
+    }
+
+    @Test
     void contextDisplayCanBeEnabledWithoutAvailabilityOrChangingExistingFlags() {
         var properties = new EnhancementProperties();
         properties.setPersistentPickupContext(true);
